f91481172f538e55724bca030f5fce7a95dd4aa5
srinivasraogp <srao79@gmail.com>
2019-05-01 18:52:22 +0530

    Initial draft

---
+++ src/main/java/com/sri/ex/BranchingStrategy1Application.java
@@ -0,0 +1,13 @@
+package com.sri.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchingStrategy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchingStrategy1Application.class, args);
+	}
+
+}
